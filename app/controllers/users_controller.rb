5d8ccbcc6f1ad73653f05fe7d1ec3e79891550b1
siydec <97608879+siydec@users.noreply.github.com>
2022-02-15 10:42:13 +0000

    update_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(current_user[:id])
+    @user.update(user_params)
+    redirect_to about_path
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+
+  end
 
   def book_params
     params.require(:book).permit(:title, :body)
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      profile_image.variant(resize: size).precessed
+      profile_image.variant(resize: size).processed
   end
 
 

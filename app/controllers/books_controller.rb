e88f8da8808dacdfde10f38a5035ddf4b8bd8ea4
siydec <97608879+siydec@users.noreply.github.com>
2022-02-23 14:23:03 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(current_user[:id])
+    @user = @book.user
     @book_new = Book.new
 
 
